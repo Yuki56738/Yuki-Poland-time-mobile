12bd4314fa721b16548493677cc67af867b80995
Yuki <enginner@risaton.net>
2022-07-06 03:51:19 +0900

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<!--  <img alt="Vue logo" src="./assets/logo.png">-->
+<!--  <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+
+  <p><button v-on:click="onClickButton()">Get Poland time</button></p>
+  <span>{{ $data.ptime }}</span>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+// eslint-disable-next-line no-unused-vars
+
+import axios from "axios";
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      ptime: null
+    }
+  },
+  methods: {
+    onClickButton: function () {
+      let data = this.$data
+      data.ptime = 'Requesting data...'
+
+      axios.get('https://flask-yuki-1.herokuapp.com/poland')
+          .then(res =>{
+            data.ptime = res.data
+          })
+    }
   }
+  // components: {
+  //   HelloWorld
+  // }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import store from './store'
+import axios from 'axios' //追記
+import VueAxios from 'vue-axios' //追記
 
-createApp(App).use(store).mount('#app')
+
+createApp(App).use(store, VueAxios, axios).mount('#app')
